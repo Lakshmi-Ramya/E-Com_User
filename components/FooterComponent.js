cddeedf434c342f71b40696d7a6e7926f6aaca7a
Akhpie <akhya1819@gmail.com>
2023-10-11 14:40:47 +0530

    commits

--- components/FooterComponent.js
+++ components/FooterComponent.js
@@ -3,10 +3,10 @@ import React from "react";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import SearchIcon from '@mui/icons-material/Search';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import PlaceIcon from '@mui/icons-material/Place';
-import InfoIcon from '@mui/icons-material/Info';
+import SearchIcon from "@mui/icons-material/Search";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import PlaceIcon from "@mui/icons-material/Place";
+import InfoIcon from "@mui/icons-material/Info";
 
 const Footer = styled.footer`
   background-color: #222;
@@ -57,7 +57,7 @@ const AdditionalSection = styled.div`
   img {
     margin-right: 8px;
   }
-  a{
+  a {
     text-decoration: none;
     color: #aaa;
     font-size: 16px;
@@ -74,25 +74,43 @@ export default function FooterComponent() {
         <FooterSection>
           <h4>About Us</h4>
           <ul>
-            <li><a href="#">Company</a></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">Press Releases</a></li>
+            <li>
+              <a href="#">Company</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">Press Releases</a>
+            </li>
           </ul>
         </FooterSection>
         <FooterSection>
           <h4>Customer Service</h4>
           <ul>
-            <li><a href="#">Help & Contact</a></li>
-            <li><a href="#">Returns & Refunds</a></li>
-            <li><a href="#">Online Orders</a></li>
+            <li>
+              <a href="#">Help & Contact</a>
+            </li>
+            <li>
+              <a href="#">Returns & Refunds</a>
+            </li>
+            <li>
+              <a href="#">Online Orders</a>
+            </li>
           </ul>
         </FooterSection>
         <FooterSection>
           <h4>Explore</h4>
           <ul>
-            <li><a href="#">Deals & Promotions</a></li>
-            <li><a href="#">Gift Cards</a></li>
-            <li><a href="#">Best Sellers</a></li>
+            <li>
+              <a href="#">Deals & Promotions</a>
+            </li>
+            <li>
+              <a href="#">Gift Cards</a>
+            </li>
+            <li>
+              <a href="#">Best Sellers</a>
+            </li>
           </ul>
         </FooterSection>
         <FooterSection>
@@ -100,12 +118,12 @@ export default function FooterComponent() {
           <ul>
             <li>
               <a href="#">
-                <FacebookIcon/> Facebook
+                <FacebookIcon /> Facebook
               </a>
             </li>
             <li>
               <a href="#">
-                <TwitterIcon/> Twitter
+                <TwitterIcon /> Twitter
               </a>
             </li>
             <li>
@@ -117,24 +135,29 @@ export default function FooterComponent() {
         </FooterSection>
       </FooterContainer>
       <AdditionalSection>
-        <StorefrontIcon style={{ color: "gold", fontSize: "16px" }} /> 
+        <StorefrontIcon style={{ color: "gold", fontSize: "16px" }} />
         <a href="#">
           <span>Shop with Confidence</span>
         </a>
-        <SearchIcon style={{ color: "gold", fontSize: "16px" }}/>
+        <SearchIcon style={{ color: "gold", fontSize: "16px" }} />
         <a href="#">
           <span>Discover More</span>
         </a>
-        <PlaceIcon style={{ color: "gold", fontSize: "16px" }}/> 
+        <PlaceIcon style={{ color: "gold", fontSize: "16px" }} />
         <a href="#">
           <span>Find Us Nearby</span>
         </a>
-        <InfoIcon style={{ color: "gold", fontSize: "16px" }}/> 
+        <InfoIcon style={{ color: "gold", fontSize: "16px" }} />
         <a href="#">
           <span>Help Center</span>
         </a>
-        <span className="licence">© 20023-2025 <span>E-Commerce</span></span>
-        <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/payment-method-c454fb.svg" alt="Payment methods" />
+        <span className="licence">
+          © 20023-2025 <span>E-Commerce</span>
+        </span>
+        <img
+          src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/payment-method-c454fb.svg"
+          alt="Payment methods"
+        />
       </AdditionalSection>
     </Footer>
   );
