bdd00c0e3a54e6b1a985cf5dbceebfd9e1edc732
Lakshmi-Ramya <vemlara177@gmail.com>
2023-10-22 17:44:04 +0530

    pushagain

--- components/FooterComponent.js
+++ components/FooterComponent.js
@@ -152,7 +152,7 @@ export default function FooterComponent() {
           <span>Help Center</span>
         </a>
         <span className="licence">
-          © 20023-2025 <span>E-Commerce</span>
+          © 2023-2025 <span>E-Commerce</span>
         </span>
         <img
           src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/payment-method-c454fb.svg"
